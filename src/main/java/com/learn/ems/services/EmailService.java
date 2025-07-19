faeaf2f8558c2ffd4c76f19e9381f48bbae6ba95
Prince Kumar Prasad <prince08833@gmail.com>
2025-07-19 08:55:58 +0530

    docs: add interface doc for email service

--- src/main/java/com/learn/ems/services/EmailService.java
+++ src/main/java/com/learn/ems/services/EmailService.java
@@ -1,5 +1,15 @@
 package com.learn.ems.services;
 
+/**
+ * Service interface for managing emails within the application.
+ */
 public interface EmailService {
+
+    /**
+     * Send email to the user
+     * @param to - the email of destination
+     * @param subject - the subject of the email
+     * @param body - body of the email, contain html language
+     */
     void sendEmail(String to, String subject, String body);
 }
